8a2341898370cb11164ab6b82988cf912eaf43fb
yywpc <770679957@qq.com>
2020-12-15 21:38:29 +0800

    项目构建.

---
+++ lushanwenku-dao/src/test/java/com/lushanwenku/dao/LushanwenkuDaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lushanwenku.dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LushanwenkuDaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ lushanwenku-guji/src/test/java/com/lushanwenku/guji/GujiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lushanwenku.guji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GujiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
